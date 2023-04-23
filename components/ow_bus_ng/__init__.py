91c2d512eedef91364dcfa1d747623b4b42f920a
ashp8i <ashp8@hotmail.com>
2023-04-23 23:08:41 +0100

    init update

--- components/ow_bus_ng/__init__.py
+++ components/ow_bus_ng/__init__.py
@@ -16,36 +16,27 @@ ESPHomeOneWireNGComponent = ow_bus_ng_ns.class_(
     "ESPHomeOneWireNGComponent", cg.Component
 )
 
-pin_schema = vol.Schema(
-    {
-        vol.Required("input_pin"): pins.gpio_input_pin_schema,
-        vol.Required("output_pin"): pins.gpio_output_pin_schema,
-    }
-)
-
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(ESPHomeOneWireNGComponent),
-        cv.Required(CONF_PIN): vol.Any(
-            [pins.gpio_input_pin_schema, pin_schema],
-            msg="The 'pin' option is required (as marked Required in the config schema) for ow_bus_ng component instance ${instance_number}. Please provide a GPIO pin number or pin object with 'input_pin' and 'output_pin'.",
-        ),
-    }
+        cv.Optional(CONF_PIN): pins.gpio_input_pin_schema,
+        cv.Optional(CONF_INPUT_PIN): pins.gpio_input_pin_schema,
+        cv.Optional(CONF_OUTPUT_PIN): pins.gpio_output_pin_schema,
+    } 
 )
 
-
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], ESPHomeOneWireNGComponent())
+  var = cg.new_Pvariable(config[CONF_ID], ESPHomeOneWireNGComponent())
+  
+  if CONF_INPUT_PIN in config: 
+    in_pin = await cg.gpio_pin_expression(config[CONF_INPUT_PIN])
+  if CONF_OUTPUT_PIN in config:
+    out_pin = await cg.gpio_pin_expression(config[CONF_OUTPUT_PIN])
+  if CONF_INPUT_PIN in config and CONF_OUTPUT_PIN in config:
+    cg.add(var.set_split_io(in_pin, out_pin))
+  else:  
+    # Single pin mode
+    pin = await cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_single_pin(pin))  
 
-    conf_pin = config[CONF_PIN]
-    if isinstance(conf_pin, dict):
-        # Split IO mode
-        in_pin = await cg.gpio_pin_expression(conf_pin["input_pin"])
-        out_pin = await cg.gpio_pin_expression(conf_pin["output_pin"])
-        cg.add(var.set_split_io(in_pin, out_pin))
-    else:
-        # Single pin mode
-        pin = await cg.gpio_pin_expression(conf_pin)
-        cg.add(var.set_single_pin(pin))
-
-    await cg.register_component(var, config)
+  await cg.register_component(var, config) 
