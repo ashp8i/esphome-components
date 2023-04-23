978a0578060b2e2fcca68f60a8f20bf3aa1f7fdf
ashp8i <ashp8@hotmail.com>
2023-04-23 14:29:54 +0100

    Update Header
    Update Header

--- components/ow_bus_ng/ow_bus_component.h
+++ components/ow_bus_ng/ow_bus_component.h
@@ -7,38 +7,72 @@
 namespace esphome {
 namespace ow_bus_ng {
 
-enum class OneWireBusPinConfig {
-  ONEWIRE_BUS_PIN_SINGLE,
-  ONEWIRE_BUS_PIN_SPLIT
-};
+// enum class OneWireBusPinConfig {
+//   ONEWIRE_BUS_PIN_SINGLE,
+//   ONEWIRE_BUS_PIN_SPLIT
+// };
+
+// class ESPHomeOneWireNGComponent : public Component {
+//  public:
+//   ESPHomeOneWireNGComponent(OneWireBusPinConfig pin_config, InternalGPIOPin *input_pin, InternalGPIOPin *output_pin,
+//                             InternalGPIOPin *pin)
+//       : pin_{pin}, input_pin_{input_pin}, output_pin_{output_pin}, split_io_{pin_config} {}
+//   virtual ~ESPHomeOneWireNGComponent();
+//   void setup() override;
+//   void dump_config() override;
+//   void update();
+
+//   void set_pin(InternalGPIOPin *pin);
+//   void set_in_pin(InternalGPIOPin *in_pin);
+//   void set_out_pin(InternalGPIOPin *out_pin);
+//   void set_split_io(OneWireBusPinConfig split_io);
+
+//   void issue_reset();
+//   void issue_skip_rom();
+//   void issue_command(uint8_t);
+
+//  protected:
+//   OneWireBusPinConfig split_io_;
+//   InternalGPIOPin *pin_{nullptr};
+//   InternalGPIOPin *input_pin_{nullptr};
+//   InternalGPIOPin *output_pin_{nullptr};
+
+//   void InitOWBus();
+// };
 
 class ESPHomeOneWireNGComponent : public Component {
  public:
-  ESPHomeOneWireNGComponent(OneWireBusPinConfig pin_config, InternalGPIOPin *input_pin, InternalGPIOPin *output_pin,
-                            InternalGPIOPin *pin)
-      : pin_{pin}, input_pin_{input_pin}, output_pin_{output_pin}, split_io_{pin_config} {}
-  virtual ~ESPHomeOneWireNGComponent();
-  void setup() override;
-  void dump_config() override;
-  void update();
+  OneWire() : OneWire(nullptr, OneWire::SINGLE_PIN) {}  // Default to single pin
 
-  void set_pin(InternalGPIOPin *pin);
-  void set_in_pin(InternalGPIOPin *in_pin);
-  void set_out_pin(InternalGPIOPin *out_pin);
-  void set_split_io(OneWireBusPinConfig split_io);
+  OneWire(InternalGPIOPin *pin) : OneWire(pin, OneWire::SINGLE_PIN) {}
 
-  void issue_reset();
-  void issue_skip_rom();
-  void issue_command(uint8_t);
+  OneWire(InternalGPIOPin *input_pin, InternalGPIOPin *output_pin)
+      : pin_config_(OneWire::SPLIT_IO), input_pin_(input_pin), output_pin_(output_pin) {}
 
- protected:
-  OneWireBusPinConfig split_io_;
-  InternalGPIOPin *pin_{nullptr};
-  InternalGPIOPin *input_pin_{nullptr};
-  InternalGPIOPin *output_pin_{nullptr};
+  void setup() override {
+    switch (this->pin_config_) {
+      case OneWire::SINGLE_PIN:
+        if (this->pin_ != nullptr) {
+          this->pin_->setup();
+          // Initialize bus and search for devices
+        }
+        break;
+      case OneWire::SPLIT_IO:
+        this->input_pin_->setup();
+        this->output_pin_->setup();
+        // Initialize bus and search for devices
+        break;
+    }
+  }
 
-  void InitOWBus();
+  enum PinConfig { SINGLE_PIN, SPLIT_IO };
+
+ private:
+  PinConfig pin_config_;
+  InternalGPIOPin *pin_;         // For SINGLE_PIN config
+  InternalGPIOPin *input_pin_;   // For SPLIT_IO config
+  InternalGPIOPin *output_pin_;  // For SPLIT_IO config
 };
 
-} // namespace ow_bus_ng
-} // namespace esphome
+}  // namespace ow_bus_ng
+}  // namespace esphome
