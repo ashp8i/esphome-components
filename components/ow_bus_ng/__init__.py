3a4a8c819ed47c706155997446178f4282ee666e
ashp8i <ashp8@hotmail.com>
2023-04-23 18:28:50 +0100

    Update __init__.py

--- components/ow_bus_ng/__init__.py
+++ components/ow_bus_ng/__init__.py
@@ -1,5 +1,5 @@
+import voluptuous as vol
 import logging
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
@@ -8,14 +8,14 @@ from esphome.const import CONF_ID, CONF_PIN
 _LOGGER = logging.getLogger(__name__)
 
 CODEOWNERS = ["@ashp8i"]
-MULTI_CONF = True
+MULTI_CONF = True 
 
 ow_bus_ng_ns = cg.esphome_ns.namespace("ow_bus_ng")
 ESPHomeOneWireNGComponent = ow_bus_ng_ns.class_(
     "ESPHomeOneWireNGComponent", cg.Component
 )
 
-pin_schema = Schema({
+pin_schema = vol.Schema({
     Required('input_pin'): pins.gpio_input_pin_schema,
     Required('output_pin'): pins.gpio_output_pin_schema 
 })
@@ -29,19 +29,3 @@ CONFIG_SCHEMA = cv.Schema(
         ])
     }
 )
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], ESPHomeOneWireNGComponent())
-    
-    conf_pin = config[CONF_PIN]
-    if isinstance(conf_pin, dict):
-        # Split IO mode
-        in_pin = await cg.gpio_pin_expression(conf_pin['input_pin'])
-        out_pin = await cg.gpio_pin_expression(conf_pin['output_pin'])
-        cg.add(var.set_split_io(in_pin, out_pin))
-    else:
-        # Single pin mode
-        pin = await cg.gpio_pin_expression(conf_pin)
-        cg.add(var.set_single_pin(pin))
-    
-    await cg.register_component(var, config)
