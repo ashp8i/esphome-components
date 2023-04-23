164bca6d70d1adf982c25f27cc191e8e13ab7943
ashp8i <ashp8@hotmail.com>
2023-04-23 23:11:40 +0100

    update

--- components/ow_bus_ng/__init__.py
+++ components/ow_bus_ng/__init__.py
@@ -19,24 +19,24 @@ ESPHomeOneWireNGComponent = ow_bus_ng_ns.class_(
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(ESPHomeOneWireNGComponent),
-        cv.Optional(CONF_PIN): pins.gpio_input_pin_schema,
-        cv.Optional(CONF_INPUT_PIN): pins.gpio_input_pin_schema,
-        cv.Optional(CONF_OUTPUT_PIN): pins.gpio_output_pin_schema,
-    } 
+        cv.Optional("pin"): pins.gpio_input_pin_schema,
+        cv.Optional("input_pin"): pins.gpio_input_pin_schema,
+        cv.Optional("output_pin"): pins.gpio_output_pin_schema,
+    }  
 )
 
 async def to_code(config):
   var = cg.new_Pvariable(config[CONF_ID], ESPHomeOneWireNGComponent())
   
-  if CONF_INPUT_PIN in config: 
-    in_pin = await cg.gpio_pin_expression(config[CONF_INPUT_PIN])
-  if CONF_OUTPUT_PIN in config:
-    out_pin = await cg.gpio_pin_expression(config[CONF_OUTPUT_PIN])
-  if CONF_INPUT_PIN in config and CONF_OUTPUT_PIN in config:
+  if "input_pin" in config: 
+    in_pin = await cg.gpio_pin_expression(config["input_pin"])
+  if "output_pin" in config:
+    out_pin = await cg.gpio_pin_expression(config["output_pin"])
+  if "input_pin" in config and "output_pin" in config:
     cg.add(var.set_split_io(in_pin, out_pin))
   else:  
     # Single pin mode
-    pin = await cg.gpio_pin_expression(config[CONF_PIN])
+    pin = await cg.gpio_pin_expression(config["pin"])
     cg.add(var.set_single_pin(pin))  
 
-  await cg.register_component(var, config) 
+  await cg.register_component(var, config)
