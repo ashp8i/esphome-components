b98a352503dd5d52d950d90e2a32c818c5728a10
ashp8i <ashp8@hotmail.com>
2023-04-24 01:39:13 +0100

    trying to fix constructor issues
    trying to fix constructor issues

--- components/ow_bus_ng/ow_bus_component.cpp
+++ components/ow_bus_ng/ow_bus_component.cpp
@@ -1,108 +1,3 @@
-// #include "ow_bus_component.h"
-// #include "esphome/core/log.h"
-// #include "esphome/core/helpers.h"
-
-// namespace esphome {
-// namespace ow_bus_ng {
-
-// static const char *const TAG = "owbus.ng";
-
-// void ESPHomeOneWireNGComponent::setup() {
-//   ESP_LOGCONFIG(TAG, "Setting up ESPHomeOneWireNGComponent...");
-
-//   if (this->split_io_ == OneWireBusPinConfig::ONEWIRE_BUS_PIN_SINGLE) {
-//     if (this->pin_ != nullptr) {
-//       this->pin_->setup();
-//     }
-//   } else if (this->split_io_ == OneWireBusPinConfig::ONEWIRE_BUS_PIN_SPLIT) {
-//     this->input_pin_->setup();
-//     this->output_pin_->setup();
-//   }
-
-//   this->InitOWBus();
-
-//   // std::vector<uint64_t> raw_devices;
-// }
-
-// void ESPHomeOneWireNGComponent::InitOWBus() {
-//   ESP_LOGD(TAG, "OneWire bus initialized! Scanning for devices...");
-
-//   if (this->split_io_ == OneWireBusPinConfig::ONEWIRE_BUS_PIN_SINGLE) {
-//     if (this->pin_ != nullptr) {
-//       this->pin_->pin_mode(gpio::FLAG_INPUT | gpio::FLAG_PULLUP);
-//       delayMicroseconds(480);
-//       // Scan bus and get list of devices
-//       // raw_devices = ...
-//     }
-//   } else if (this->split_io_ == OneWireBusPinConfig::ONEWIRE_BUS_PIN_SPLIT) {
-//     this->input_pin_->pin_mode(gpio::FLAG_INPUT);
-//     this->output_pin_->pin_mode(gpio::FLAG_OUTPUT);
-//     // Scan bus and get list of devices
-//     // raw_devices = ...
-//   }
-// }
-
-// void ESPHomeOneWireNGComponent::set_split_io(OneWireBusPinConfig split_io) {
-//   if (split_io != this->split_io_) {
-//     this->split_io_ = split_io;
-
-//     if (this->split_io_ == OneWireBusPinConfig::ONEWIRE_BUS_PIN_SINGLE) {
-//       if (this->pin_ != nullptr) {
-//         this->pin_->pin_mode(gpio::FLAG_INPUT | gpio::FLAG_PULLUP);
-//       }
-//     }
-
-//     else if (this->split_io_ == OneWireBusPinConfig::ONEWIRE_BUS_PIN_SPLIT && this->input_pin_ != nullptr &&
-//              this->output_pin_ != nullptr) {
-//       this->input_pin_->pin_mode(gpio::FLAG_INPUT);
-//       this->output_pin_->pin_mode(gpio::FLAG_OUTPUT);
-//     }
-
-//     ESP_LOGD(TAG, "Set split_io to %d", static_cast<int>(split_io));
-//   }
-// }
-
-// void ESPHomeOneWireNGComponent::dump_config() {
-//   ESP_LOGD(TAG, "Configuration:");
-//   ESP_LOGD(TAG, "  split_io: %d", static_cast<int>(this->split_io_));
-//   if (this->pin_ != nullptr) {
-//     ESP_LOGD(TAG, "  pin: %d", this->pin_->get_pin());
-//   }
-//   if (this->input_pin_ != nullptr) {
-//     ESP_LOGD(TAG, "  input_pin: %d", this->input_pin_->get_pin());
-//   }
-//   if (this->output_pin_ != nullptr) {
-//     ESP_LOGD(TAG, "  output_pin: %d", this->output_pin_->get_pin());
-//   }
-// }
-
-// void ESPHomeOneWireNGComponent::set_pin(InternalGPIOPin *pin) {
-//   if (pin != nullptr) {
-//     this->pin_ = pin;
-//     this->pin_->setup();
-//     ESP_LOGD(TAG, "Set pin to %d", pin->get_pin());
-//   }
-// }
-
-// void ESPHomeOneWireNGComponent::set_in_pin(InternalGPIOPin *in_pin) {
-//   if (in_pin != nullptr) {
-//     this->input_pin_ = in_pin;
-//     this->input_pin_->setup();
-//     ESP_LOGD(TAG, "Set input_pin to %d", in_pin->get_pin());
-//   }
-// }
-
-// void ESPHomeOneWireNGComponent::set_out_pin(InternalGPIOPin *out_pin) {
-//   if (out_pin != nullptr) {
-//     this->output_pin_ = out_pin;
-//     this->output_pin_->setup();
-//     ESP_LOGD(TAG, "Set output_pin to %d", out_pin->get_pin());
-//   }
-// }
-
-// }  // namespace ow_bus_ng
-// }  // namespace esphome
-
 #include "ow_bus_component.h"
 #include "esphome/core/log.h"
 #include "esphome/core/helpers.h"
@@ -113,24 +8,21 @@ namespace ow_bus_ng {
 static const char *const TAG = "owbus.ng";
 
 void ESPHomeOneWireNGComponent::set_single_pin(InternalGPIOPin *pin) {
-  this->pin_ = pin;  // Save the single pin 
+  this->pin_ = pin;  // Save the single pin
 }
 
-void ESPHomeOneWireNGComponent::set_split_io(InternalGPIOPin *input_pin,  
-                                             InternalGPIOPin *output_pin) {
+void ESPHomeOneWireNGComponent::set_split_io(InternalGPIOPin *input_pin, InternalGPIOPin *output_pin) {
   this->input_pin_ = input_pin;  // Save the split IO pins
   this->output_pin_ = output_pin;
 }
 
 // Constructor definitions here
-ESPHomeOneWireNGComponent() : Component(), pin_config_(OneWirePinConfig::SINGLE_PIN) {}
+ESPHomeOneWireNGComponent::ESPHomeOneWireNGComponent() : pin_config_(OneWirePinConfig::SINGLE_PIN) {}
 
-ESPHomeOneWireNGComponent(InternalGPIOPin *pin) : Component(), pin_(pin), pin_config_(OneWirePinConfig::SINGLE_PIN) {}
+ESPHomeOneWireNGComponent::ESPHomeOneWireNGComponent(InternalGPIOPin *pin)
+    : pin_(pin), pin_config_(OneWirePinConfig::SINGLE_PIN) {}
 
-ESPHomeOneWireNGComponent(InternalGPIOPin *input_pin, InternalGPIOPin *output_pin)
-    : Component(), input_pin_(input_pin), output_pin_(output_pin), pin_config_(OneWirePinConfig::SPLIT_IO) {}
-
-void ESPHomeOneWireNGComponent::setup() override {
+void ESPHomeOneWireNGComponent::setup() {
   Component::setup();  // Call parent class setup()
 
   ESP_LOGCONFIG(TAG, "Setting up ESPHomeOneWireNGComponent...");
@@ -151,7 +43,7 @@ void ESPHomeOneWireNGComponent::setup() override {
   // ...
 }
 
-void ESPHomeOneWireNGComponent::dump_config() override {
+void ESPHomeOneWireNGComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "Configuration:");
 
   switch (this->pin_config_) {
--- components/ow_bus_ng/ow_bus_component.h
+++ components/ow_bus_ng/ow_bus_component.h
@@ -1,45 +1,3 @@
-// #pragma once
-
-// #include "esphome/core/hal.h"
-// #include "esphome/core/component.h"
-// #include <vector>
-
-// namespace esphome {
-// namespace ow_bus_ng {
-
-// enum class OneWireBusPinConfig {
-//   ONEWIRE_BUS_PIN_SINGLE,
-//   ONEWIRE_BUS_PIN_SPLIT
-// };
-
-// class ESPHomeOneWireNGComponent : public Component {
-//  public:
-//   ESPHomeOneWireNGComponent(OneWireBusPinConfig pin_config, InternalGPIOPin *input_pin, InternalGPIOPin *output_pin,
-//                             InternalGPIOPin *pin)
-//       : pin_{pin}, input_pin_{input_pin}, output_pin_{output_pin}, split_io_{pin_config} {}
-//   virtual ~ESPHomeOneWireNGComponent();
-//   void setup() override;
-//   void dump_config() override;
-//   void update();
-
-//   void set_pin(InternalGPIOPin *pin);
-//   void set_in_pin(InternalGPIOPin *in_pin);
-//   void set_out_pin(InternalGPIOPin *out_pin);
-//   void set_split_io(OneWireBusPinConfig split_io);
-
-//   void issue_reset();
-//   void issue_skip_rom();
-//   void issue_command(uint8_t);
-
-//  protected:
-//   OneWireBusPinConfig split_io_;
-//   InternalGPIOPin *pin_{nullptr};
-//   InternalGPIOPin *input_pin_{nullptr};
-//   InternalGPIOPin *output_pin_{nullptr};
-
-//   void InitOWBus();
-// };
-
 #pragma once
 
 #include "esphome/core/hal.h"
@@ -52,21 +10,16 @@ namespace ow_bus_ng {
 
 class ESPHomeOneWireNGComponent : public esphome::Component {
  public:
-  ESPHomeOneWireNGComponent() : pin_config_(OneWirePinConfig::SINGLE_PIN) {}  
+  ESPHomeOneWireNGComponent() : pin_config_(OneWirePinConfig::SINGLE_PIN) {}
 
-  ESPHomeOneWireNGComponent(esphome::InternalGPIOPin *pin) : pin_(pin), pin_config_(OneWirePinConfig::SINGLE_PIN) {}  
-  
-  ESPHomeOneWireNGComponent(esphome::InternalGPIOPin *input_pin, esphome::InternalGPIOPin *output_pin)  
-       : input_pin_(input_pin), output_pin_(output_pin), pin_config_(OneWirePinConfig::SPLIT_IO) {}
-        
-  void setup() override {  
-  }  
+  ESPHomeOneWireNGComponent(InternalGPIOPin *pin) : pin_(pin), pin_config_(OneWirePinConfig::SINGLE_PIN) {}
 
-  void dump_config() override {
-  }
+  void setup() override;
+
+  void dump_config() override;
   enum OneWirePinConfig { SINGLE_PIN, SPLIT_IO };
-  void set_single_pin(esphome::InternalGPIOPin *pin);
-  void set_split_io(esphome::InternalGPIOPin *input_pin, esphome::InternalGPIOPin *output_pin);
+  void set_single_pin(InternalGPIOPin *pin);
+  void set_split_io(InternalGPIOPin *input_pin, InternalGPIOPin *output_pin);
 
  protected:
   InternalGPIOPin *pin_{nullptr};
