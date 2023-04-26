befd5d46784889d4cc7cdd2981b8184c7925d96e
ashp8i <ashp8@hotmail.com>
2023-04-26 16:53:11 +0100

    Update __init__.py

--- components/ow_bus_ng/__init__.py
+++ components/ow_bus_ng/__init__.py
@@ -1,12 +1,9 @@
-# import voluptuous as vol
 import logging
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import uart
 from esphome.const import CONF_ID, CONF_PIN
-
-# from voluptuous import Required, Optional, Any, All, Lower, In
 
 DEPENDENCIES = ["uart"]
 
@@ -30,12 +27,19 @@ CONFIG_SCHEMA = cv.Schema(
             {
                 "BITBANG_SINGLE": "bitbang_single",
                 "BITBANG_SPLIT_IO": "bitbang_split_io",
-                # "UART_BUS_SINGLE_PIN": "uart_single_pin",
+                #   "UART_BUS_SINGLE_PIN": "uart_bus_single_pin",
                 "UART_BUS": "uart_bus",
             },
             upper=True,
         ),
-        cv.Optional("uart"): uart.UART_DEVICE_SCHEMA,
+        cv.Optional("uart_id"): cv.int_,
+        cv.Optional("uart"): cv.Schema(
+            {
+                cv.Required("tx_pin"): pins.gpio_output_pin_schema,
+                cv.Required("rx_pin"): pins.gpio_input_pin_schema,
+                cv.Optional("baud_rate", default="115200bps"): baud_rate_schema,
+            }
+        ),
     }
 )
 
@@ -51,17 +55,16 @@ async def setup_bitbang_split_io(var, config):
     cg.add(var.set_split_io(in_pin, out_pin))
 
 
-# async def setup_uart_single_pin(var, config):
-#     if "uart" in config:
-#         await uart.register_uart_device(var, config["uart"])
-#         uart_conf = config["uart"]
-#         cg.add(
-#             var.set_uart(
-#                 uart_conf["tx_pin"], uart_conf["rx_pin"], uart_conf["baud_rate"]
-#             )
-#         )
-#     else:
-#         _LOGGER.error("uart: not specified for uart_half_duplex mode!")
+# async def setup_uart_half_duplex(var, config):
+#   if "uart" in config:
+#     await uart.register_uart_device(var, config["uart"])
+#     uart_conf = config["uart"]
+#     cg.add(var.set_uart(uart_conf["tx_pin"], uart_conf["rx_pin"], uart_conf["baud_rate"]))
+#   elif "uart_id" in config:
+#     var.uart_bus = uart.ESP_PLATFORM.get_uart_instance(config["uart_id"])
+#   else:
+#     uart_buses = uart.ESP_PLATFORM.get_uart_instances()
+#     var.uart_bus = uart_buses[0]
 
 
 async def setup_uart_full_duplex(var, config):
@@ -73,8 +76,11 @@ async def setup_uart_full_duplex(var, config):
                 uart_conf["tx_pin"], uart_conf["rx_pin"], uart_conf["baud_rate"]
             )
         )
+    elif "uart_id" in config:
+        var.uart_bus = uart.ESP_PLATFORM.get_uart_instance(config["uart_id"])
     else:
-        _LOGGER.error("uart not provided for uart_full_duplex!")
+        uart_buses = uart.ESP_PLATFORM.get_uart_instances()
+        var.uart_bus = uart_buses[0]
 
 
 async def to_code(config):
@@ -85,9 +91,9 @@ async def to_code(config):
         await setup_bitbang_single(var, config)
     elif mode == "bitbang_split_io":
         await setup_bitbang_split_io(var, config)
-    #   elif mode == "uart_single_pin" and "uart" in config:
-    #     await setup_uart_single_pin(var, config)
-    elif mode == "uart_full_duplex" and "uart" in config:
+    #   elif mode == "uart_bus_single_pin":
+    #     await setup_uart_half_duplex(var, config)
+    elif mode == "uart_bus":
         await setup_uart_full_duplex(var, config)
     else:
         _LOGGER.error("Invalid mode for single pin: %s", mode)
