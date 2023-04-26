1faea3719fafe449b47f5f7ad4698f196a652b01
ashp8i <ashp8@hotmail.com>
2023-04-26 11:43:25 +0100

    Update __init__.py

--- components/ow_bus_ng/__init__.py
+++ components/ow_bus_ng/__init__.py
@@ -49,9 +49,9 @@ async def to_code(config):
       if mode == "bitbang_single":
         # Existing logic
       elif mode == "bitbang_split_io":
-        in_pin = await cg.gpio_pin_expression(config["input_pin"])
-        out_pin = await cg.gpio_pin_expression(config["output_pin"])
-        cg.add(var.set_split_io(in_pin, out_pin))
+          in_pin = await cg.gpio_pin_expression(config["input_pin"])
+          out_pin = await cg.gpio_pin_expression(config["output_pin"])
+          cg.add(var.set_split_io(in_pin, out_pin))
       # elif mode == "uart_half_duplex":
       #   cg.add(var.set_uart_half_duplex(uart_conf["tx_pin"], uart_conf["rx_pin"], uart_conf["baud_rate"]))
       elif mode == "uart_full_duplex":
