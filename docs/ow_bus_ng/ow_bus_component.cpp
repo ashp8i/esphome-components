7504a502f053ec67b1465e677157deb6b36cb105
ashp8i <ashp8@hotmail.com>
2023-05-01 06:10:18 +0100

    Update ow_bus_component.cpp

--- docs/ow_bus_ng/ow_bus_component.cpp
+++ docs/ow_bus_ng/ow_bus_component.cpp
@@ -7,156 +7,156 @@ namespace ow_bus_ng {
 
 static const char *const TAG = "owbus.ng";
 
-// Bit bang driver info struct 
-struct OneWireBBDriverInfo {
-  gpio_num_t gpio;    // 1-Wire bus GPIO
-  bool bus_state;     // Current bus level (high/low)
-  uint8_t tx_buffer;  // Current TX byte
-  int tx_bits_left;   // Bits remaining to TX in current byte
-  uint8_t rx_buffer;  // Current RX byte
-  int rx_bits_left;   // Bits remaining to RX in current byte 
-};
+// // Bit bang driver info struct 
+// struct OneWireBBDriverInfo {
+//   gpio_num_t gpio;    // 1-Wire bus GPIO
+//   bool bus_state;     // Current bus level (high/low)
+//   uint8_t tx_buffer;  // Current TX byte
+//   int tx_bits_left;   // Bits remaining to TX in current byte
+//   uint8_t rx_buffer;  // Current RX byte
+//   int rx_bits_left;   // Bits remaining to RX in current byte 
+// };
 
-// Initialize driver
-void oneWireBBDInit(OneWireBBDriverInfo *info, gpio_num_t gpio) {
-  info->gpio = gpio;
-  gpio_set_direction(gpio, GPIO_MODE_OUTPUT);   // Set as output
-  gpio_set_level(gpio, 1);                      // Set high (idle)
-  info->bus_state = true;
-}
+// // Initialize driver
+// void oneWireBBDInit(OneWireBBDriverInfo *info, gpio_num_t gpio) {
+//   info->gpio = gpio;
+//   gpio_set_direction(gpio, GPIO_MODE_OUTPUT);   // Set as output
+//   gpio_set_level(gpio, 1);                      // Set high (idle)
+//   info->bus_state = true;
+// }
 
-// Transmit a bit 
-void oneWireBBTransmitBit(OneWireBBDriverInfo *info, bool bit) {
-  if (bit) {
-    gpio_set_level(info->gpio, 0);               // Set low 
-  } else {
-    gpio_set_level(info->gpio, 1);               // Set high
-  }
-  info->bus_state = !bit;                       // Toggle bus state
+// // Transmit a bit 
+// void oneWireBBTransmitBit(OneWireBBDriverInfo *info, bool bit) {
+//   if (bit) {
+//     gpio_set_level(info->gpio, 0);               // Set low 
+//   } else {
+//     gpio_set_level(info->gpio, 1);               // Set high
+//   }
+//   info->bus_state = !bit;                       // Toggle bus state
   
-  // Delay for ~5us
-  delayMicroseconds(5);  
+//   // Delay for ~5us
+//   delayMicroseconds(5);  
   
-  gpio_set_level(info->gpio, 1);                 // Idle bus high   
-}
+//   gpio_set_level(info->gpio, 1);                 // Idle bus high   
+// }
 
-// Receive a bit  
-bool oneWireBBReceiveBit(OneWireBBDriverInfo *info) { 
-  gpio_set_direction(info->gpio, GPIO_MODE_INPUT);    // Set as input
+// // Receive a bit  
+// bool oneWireBBReceiveBit(OneWireBBDriverInfo *info) { 
+//   gpio_set_direction(info->gpio, GPIO_MODE_INPUT);    // Set as input
   
-  delayMicroseconds(4);                            // Sample delay
+//   delayMicroseconds(4);                            // Sample delay
   
-  bool bit = gpio_get_level(info->gpio);           // Sample level 
+//   bool bit = gpio_get_level(info->gpio);           // Sample level 
   
-  delayMicroseconds(60);                           // Recovery delay
+//   delayMicroseconds(60);                           // Recovery delay
   
-  gpio_set_direction(info->gpio, GPIO_MODE_OUTPUT); // Set as output
-  gpio_set_level(info->gpio, 1);                    // Idle bus high  
+//   gpio_set_direction(info->gpio, GPIO_MODE_OUTPUT); // Set as output
+//   gpio_set_level(info->gpio, 1);                    // Idle bus high  
   
-  return bit;
-}
+//   return bit;
+// }
 
-struct OneWireBusRMTState {
-  int tx_channel;     // RMT TX channel 
-  int rx_channel;     // RMT RX channel
-  RingbufHandle_t rb; // RX ringbuffer handle
-  int gpio;           // OneWire bus GPIO pin  
-  OneWireBus *bus;    // OneWire bus instance 
-};
+// struct OneWireBusRMTState {
+//   int tx_channel;     // RMT TX channel 
+//   int rx_channel;     // RMT RX channel
+//   RingbufHandle_t rb; // RX ringbuffer handle
+//   int gpio;           // OneWire bus GPIO pin  
+//   OneWireBus *bus;    // OneWire bus instance 
+// };
 
-OneWireBus *oneWireBusRMTInit(OneWireBusRMTState *state, int gpio, 
-                               int tx_channel, int rx_channel);
+// OneWireBus *oneWireBusRMTInit(OneWireBusRMTState *state, int gpio, 
+//                                int tx_channel, int rx_channel);
 
-// Constructor definitions here
-ESPHomeOneWireNGComponent::ESPHomeOneWireNGComponent() {}
+// // Constructor definitions here
+// ESPHomeOneWireNGComponent::ESPHomeOneWireNGComponent() {}
 
-ESPHomeOneWireNGComponent::ESPHomeOneWireNGComponent(InternalGPIOPin *pin) {}
+// ESPHomeOneWireNGComponent::ESPHomeOneWireNGComponent(InternalGPIOPin *pin) {}
 
-void ESPHomeOneWireNGComponent::setup() { 
-  ESP_LOGCONFIG(TAG, "Setting up ESPHomeOneWireNGComponent...");
-  ESP_LOGCONFIG(TAG, "Setting up RMT/bitbang single pin mode...");
-  if (this->pin_ != nullptr) {
-    if (!perform_reset())
-      return;  // No device present
-      this->configure_rmt_(); 
-  }
-}
+// void ESPHomeOneWireNGComponent::setup() { 
+//   ESP_LOGCONFIG(TAG, "Setting up ESPHomeOneWireNGComponent...");
+//   ESP_LOGCONFIG(TAG, "Setting up RMT/bitbang single pin mode...");
+//   if (this->pin_ != nullptr) {
+//     if (!perform_reset())
+//       return;  // No device present
+//       this->configure_rmt_(); 
+//   }
+// }
 
-void RemoteTransmitterComponent::configure_rmt_() {
-  rmt_config_t c{};
+// void RemoteTransmitterComponent::configure_rmt_() {
+//   rmt_config_t c{};
 
-  this->config_rmt(c);
-  c.rmt_mode = RMT_MODE_TX;
-  c.gpio_num = gpio_num_t(this->pin_->get_pin());
+//   this->config_rmt(c);
+//   c.rmt_mode = RMT_MODE_TX;
+//   c.gpio_num = gpio_num_t(this->pin_->get_pin());
 
-  c.rx_config.enable = false;  // Disable RMT RX
-  c.tx_config.loop_en = false;
-  c.tx_config.carrier_en = true; 
-  c.tx_config.idle_output_en = true;
+//   c.rx_config.enable = false;  // Disable RMT RX
+//   c.tx_config.loop_en = false;
+//   c.tx_config.carrier_en = true; 
+//   c.tx_config.idle_output_en = true;
   
-  c.tx_config.carrier_freq_hz = 38000;  // 1-Wire frequency 
-  c.tx_config.carrier_duty_percent = 50; 
+//   c.tx_config.carrier_freq_hz = 38000;  // 1-Wire frequency 
+//   c.tx_config.carrier_duty_percent = 50; 
    
-  c.clk_div = 80; 
+//   c.clk_div = 80; 
    
-  c.tx_config.idle_output_en = true;
-  c.tx_config.carrier_level = RMT_CARRIER_LEVEL_HIGH;
-  c.tx_config.idle_level = RMT_IDLE_LEVEL_LOW; 
+//   c.tx_config.idle_output_en = true;
+//   c.tx_config.carrier_level = RMT_CARRIER_LEVEL_HIGH;
+//   c.tx_config.idle_level = RMT_IDLE_LEVEL_LOW; 
 
-  esp_err_t error = rmt_config(&c);
-  if (error != ESP_OK) {
-    this->error_code_ = error;
-    this->mark_failed();
-    return;
-  }
+//   esp_err_t error = rmt_config(&c);
+//   if (error != ESP_OK) {
+//     this->error_code_ = error;
+//     this->mark_failed();
+//     return;
+//   }
   
-  error = rmt_driver_install(this->channel_, &c.tx_config, 0); 
-                               // Pass TX config only
-  if (error != ESP_OK) {
-    this->error_code_ = error;
-    this->mark_failed();
-    return;
-  }
-  this->initialized_ = true; 
-}
+//   error = rmt_driver_install(this->channel_, &c.tx_config, 0); 
+//                                // Pass TX config only
+//   if (error != ESP_OK) {
+//     this->error_code_ = error;
+//     this->mark_failed();
+//     return;
+//   }
+//   this->initialized_ = true; 
+// }
 
-// To receive:
-rmt_driver_uninstall(this->channel_);  // Free GPIO pin  
-uint8_t received = gpio_get_level(c.gpio_num);
+// // To receive:
+// rmt_driver_uninstall(this->channel_);  // Free GPIO pin  
+// uint8_t received = gpio_get_level(c.gpio_num);
 
-rmt_driver_install(this->channel_, &c.tx_config, 0);
+// rmt_driver_install(this->channel_, &c.tx_config, 0);
 
-void ESPHomeOneWireNGComponent::dump_config() {
-  ESP_LOGCONFIG(TAG, "  Using bitbang single pin mode:");
-  ESP_LOGCONFIG(TAG, "    Pin: %d", this->pin_->get_pin());
-}
+// void ESPHomeOneWireNGComponent::dump_config() {
+//   ESP_LOGCONFIG(TAG, "  Using bitbang single pin mode:");
+//   ESP_LOGCONFIG(TAG, "    Pin: %d", this->pin_->get_pin());
+// }
 
-bool ESPHomeOneWireNGComponent::perform_reset() {
-  if (this->pin_ != nullptr) {
-    this->pin_->digital_write(false);
-    delayMicroseconds(480);
-    this->pin_->digital_write(false);
-    delayMicroseconds(70);
-    // Method logic...
-    return true;  // Add return statement
-  } else
-    return false;  // Add return statement
-}
+// bool ESPHomeOneWireNGComponent::perform_reset() {
+//   if (this->pin_ != nullptr) {
+//     this->pin_->digital_write(false);
+//     delayMicroseconds(480);
+//     this->pin_->digital_write(false);
+//     delayMicroseconds(70);
+//     // Method logic...
+//     return true;  // Add return statement
+//   } else
+//     return false;  // Add return statement
+// }
 
-// Definitions
+// // Definitions
 
-class OWNGBus : public Component { /* ... */ }; 
+// class OWNGBus : public Component { /* ... */ }; 
 
-class OneWireBBComponent : public OneWireBus {
- public:
-  OneWireBBComponent(PinMode pin_mode, ...) { /* ... */ }
+// class OneWireBBComponent : public OneWireBus {
+//  public:
+//   OneWireBBComponent(PinMode pin_mode, ...) { /* ... */ }
   
-  void setup() override { ... }
+//   void setup() override { ... }
   
-  // Other OneWireBus methods...
-};
+//   // Other OneWireBus methods...
+// };
 
-class OneWireRMTCComponent : public OneWireBus { /* ... */ };
+// class OneWireRMTCComponent : public OneWireBus { /* ... */ };
 
 }  // namespace ow_bus_ng
 }  // namespace esphome
